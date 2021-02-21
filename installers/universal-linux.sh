8b2dbb5d708e73c0a1b5eeb8750f9c3767ab3e9d
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-02-21 17:22:48 +0100

    Create universal-linux.sh

---
+++ installers/universal-linux.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "
+////////////////
+//    ////  ///
+///    //   ///
+////    /   ///
+/////       ///
+///////////////"
+
+wget https://raw.githubusercontent.com/Uncoded-xyz/VirgoScript-Gen/main/src/loader.php
+
+echo "Thanks to install VirgoScript Generator!"
